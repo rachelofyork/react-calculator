b0a8859231246951fa5259299776144a308e857d
rachelofyork <rachelofyork@gmail.com>
2022-02-06 12:32:53 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="App">
+      Hello World!
+    </div>
+    <footer><em>Coded by <a href="https://www.rachelofyork.com" target="_blank" rel="noreferrer" 
+    alt="Link to Rachel Hollingworth's website">Rachel Hollingworth</a></em></footer>
     </div>
   );
 }

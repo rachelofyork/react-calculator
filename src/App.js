3563ae54c567546e736f3afe49debe43aefdee50
rachelofyork <rachelofyork@gmail.com>
2022-02-07 14:32:12 +0000

    Added Clear Button component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
-import { useState } from "react";
+import { ClearButton } from "./components/ClearButton";
 
 class App extends Component {
   constructor (props){
@@ -14,7 +14,7 @@ class App extends Component {
   return (
     <div className="App">
      <div className="container">
-       <Input input={this.state.input}></Input>
+       <Input input={this.state.input} />
       <div className="row">
         <Button>7</Button>
         <Button>8</Button>
@@ -38,6 +38,9 @@ class App extends Component {
         <Button>0</Button>
         <Button>=</Button>
         <Button>-</Button>
+      </div>
+      <div className="row">
+<ClearButton handleClear={()=> this.setState({input: ""})}>Clear</ClearButton>
       </div>
     </div>
     </div>

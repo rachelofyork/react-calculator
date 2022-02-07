cef443ae606875dda0da1fe5d7b4e0ce03a3a400
rachelofyork <rachelofyork@gmail.com>
2022-02-07 12:42:38 +0000

    Added Input component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Button from "./components/Button";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import { useState } from "react";
 
-function App() {
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state ={input: ""}
+  }
+  render(){
   return (
     <div className="App">
      <div className="container">
+       <Input input={this.state.input}></Input>
       <div className="row">
         <Button>7</Button>
         <Button>8</Button>
@@ -34,6 +42,7 @@ function App() {
     </div>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./Button.css";
 
 
-const isOperator = val => {
+ const isOperator = val => {
     return !isNaN(val) || val === "." || val ==="=";
 }
 //above means if value is a number, ., or =, it will return true
 // !isNaN is double negative meaning 'is a number' 
 
 
-const Button = props => (
+export const Button = props => (
 <div className={`button-container ${isOperator(props.children) ? null : "operator"}`}>
     {props.children}
 </div>
@@ -17,4 +17,3 @@ const Button = props => (
 //code inside ${} above means: if isOperator is true (i.e. it's a number, . or =), it'll do nothing, but(:)if it's 
 //true it will add operator class
 
-export default Button;

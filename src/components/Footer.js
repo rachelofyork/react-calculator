99801cb1ba5586c6ce0b495e225318b436e9ab8d
rachelofyork <rachelofyork@gmail.com>
2022-02-13 13:01:05 +0000

    Added GitHub link to Footer, changed colours

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,5 +3,6 @@ import "./Footer.css";
 
 export const Footer = () => 
      (<footer><em>Coded by <a href="https://www.rachelofyork.com" target="_blank" rel="noreferrer" 
-    alt="Link to Rachel Hollingworth's website">Rachel Hollingworth</a></em></footer> 
+    alt="Link to Rachel Hollingworth's website">Rachel Hollingworth</a><br/><a href="https://github.com/rachelofyork/react-calculator" target="_blank" rel="noreferrer" 
+    alt="Link to this project's code">Code on GitHub</a></em></footer> 
     )
